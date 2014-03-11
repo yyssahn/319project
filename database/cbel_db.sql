720654798a75efaecfc5580fab3003ba5d70de09
Kieran Harrison <h7r8@ugrad.cs.ubc.ca>
2014-03-10 22:59:12 -0700

    Updated database

--- database/cbel_db.sql
+++ database/cbel_db.sql
@@ -1,51 +1,52 @@
-CREATE TABLE IF NOT EXISTS CategoryOptions(
-	iid int NOT NULL PRIMARY KEY,
-	idea_type varchar(50),
-	referral varchar(50),
-	mandate varchar(50),
-	focus varchar(50),
-	main_activities varchar(50),
-	delivery_location varchar(50),
-	disciplines varchar(50),
-	timeframe varchar(50),
-	status varchar(50)
-);
+CREATE DATABASE  IF NOT EXISTS `cbel_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `cbel_db`;
+-- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+--
+-- Host: 127.0.0.1    Database: cbel_db
+-- ------------------------------------------------------
+-- Server version	5.6.16
 
-INSERT INTO CategoryOptions values(
-	1,
-	'One-Time Project',
-	'Course-Based Opportunity',
-	'Aboriginal Engagement',
-	'Aboriginal Engagement',
-	'Consultation',
-	NULL,
-	NULL,
-	NULL,
-	'Initial Idea Inputted'	
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO CategoryOptions values(
-	2,
-	'Recurring Project',
-	'Trek Program',
-	'Arts - Culture - Heritage',
-	'Arts - Culture - Heritage',
-	'Curriculum Development',
-	NULL,
-	NULL,
-	NULL,
-	'Assigned Ownership'
-);
+--
+-- Table structure for table `categoryoptions`
+--
 
-INSERT INTO CategoryOptions values(
-	3,
-	'Part of a Multi-Phase Project',
-	'Reading Week Project',
-	'Civic Participation - Politics - Democracy',
-	'Civic Participation - Politics - Democracy',
-	'Data Gathering and Mapping',
-	NULL,
-	NULL,
-	NULL,
-	'Active Development'
-);
+DROP TABLE IF EXISTS `categoryoptions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `categoryoptions` (
+  `iid` int(11) NOT NULL AUTO_INCREMENT,
+  `idea_type` varchar(45) DEFAULT NULL,
+  `referral` varchar(45) DEFAULT NULL,
+  `mandate` varchar(45) DEFAULT NULL,
+  `focus` varchar(45) DEFAULT NULL,
+  `main_activities` varchar(45) DEFAULT NULL,
+  `location` varchar(45) DEFAULT NULL,
+  `disciplines` varchar(45) DEFAULT NULL,
+  `timeframe` varchar(45) DEFAULT NULL,
+  `status` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`iid`),
+  UNIQUE KEY `iid_UNIQUE` (`iid`)
+) ENGINE=InnoDB AUTO_INCREMENT=114 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2014-03-10 22:58:49
