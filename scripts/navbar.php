d8365ec55f6bde80b2b1b5c1ccfa8d6dec40f200
unknown <s6t7@ugrad.cs.ubc.ca>
2014-03-11 17:05:29 -0400

    Navbar update

--- scripts/navbar.php
+++ scripts/navbar.php
@@ -1,13 +1,47 @@
 <nav class="navbar navbar-default" role="navigation">
-	<div class="container-fluid">
-		<!-- Collect the nav links, forms, and other content for toggling -->
-		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			  <ul class="nav navbar-nav">
-					<li><a href="index.php?content=home">Home</a></li>
-					<li><a href="index.php?content=leads">Leads <span class="badge">14</span></a></li> 
-					<li><a href="index.php?content=notifications">Notifications</a></li> 
-					<li><a href="index.php?content=settings">Settings</a></li> 
-			  </ul>
-		</div><!-- /.navbar-collapse -->
-	</div><!-- /.container-fluid -->
+	<div class="row clearfix">
+		<div class="col-md-12 column">
+			<nav class="navbar navbar-default navbar-inverse" role="navigation">
+				<div class="navbar-header">
+					 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="index.php?content=home">Home</a>
+				</div>
+				
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav">
+							<div class="navbar-header">
+									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="index.php?content=leads">Leads</a>
+							</div>
+
+							<div class="navbar-header">
+									<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button> <a class="navbar-brand" href="index.php?content=notifications">Notifications</a>
+							</div>
+
+							<li class="drop-down">
+							 <a href="#" class="dropdown-toggle" class="navbar-toggle" data-toggle="dropdown">Setting<strong class="caret"></strong></a>
+								<ul class="dropdown-menu">
+								
+									<li>
+										<a href="index.php?content=settings">Change profile</a>
+									</li>
+
+									<li class="divider"></li>
+
+									<li>
+										<a href="#">Log out</a>
+									</li>
+								</ul>
+							</li>
+					</ul>
+
+					<form class="navbar-form navbar-left" style = "margin-left: 469px" role="search">
+						<div class="form-group">
+							<input type="text" class="form-control">
+						</div> 
+						<button type="submit" class="btn btn-default">Search</button>
+					</form>
+				
+				</div>
+			</nav>
+		</div>
+	</div>
 </nav>
