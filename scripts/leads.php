087f0ef90c31b8d55ce85b65489798bace57dd29
Yoonsung Ahn <yyssahn@hotmail.com>
2014-03-20 11:32:27 -0700

    search done, cannot continue without db update

--- scripts/leads.php
+++ scripts/leads.php
@@ -4,16 +4,101 @@
 
 <?php
 	include('database_helper.php');
+	
+		$db = new DatabaseHelper();
+
 	if(isset($_POST['submit'])){
-		echo "Input submitted mother fucker!";
+			$query = "SELECT idea_name FROM cbel_lead WHERE";
+			$client=NULL;
+			$name = NULL;
+			$type=NULL;
+			$referral=NULL;
+			$mandate=NULL;
+			$focus=NULL;
+			$activities=NULL;
+			$delivery=NULL;
+			$disciplines=NULL;
+			$timeframe=NULL;
+			$status=NULL;
+			
+			if (isset($_POST['name'])){
+				foreach($_POST['name'] as $row){
+					$query = $query." idea_name LIKE '%".$row."%' OR";	
+				}
+			}
+			if (isset($_POST['type'])){
+				foreach($_POST['type'] as $row){
+					$query = $query." idea_type LIKE '%".$row."%' OR";	
+				}
+				print_r ($type);
+			}
+				if (isset($_POST['referral'])){
+				
+				foreach($_POST['referral'] as $row){
+				$query = $query." referral LIKE '%".$row."%' OR";	
+				}
+			}
+				if (isset($_POST['mandate'])){
+				foreach($_POST['mandate'] as $row){
+			$query = $query." mandate LIKE '%".$row."%' OR";	
+			}
+			}
+				if (isset($_POST['focus'])){
+				foreach($_POST['focus'] as $row){
+			$query = $query." focus LIKE '%".$row."%' OR";	
+			}
+				print_r ($focus);
+			}
+				if (isset($_POST['activities'])){
+				foreach($_POST['activities'] as $row){
+$query = $query." main_activities LIKE '%".$row."%' OR";	
+					
+			}
+				}
+				if (isset($_POST['delivery'])){
+				foreach($_POST['delivery'] as $row){
+					$query = $query." delivery_location LIKE '%".$row."%' OR";	
+		
+					}
+			}
+				if (isset($_POST['disciplines'])){
+				foreach($_POST['disciplines'] as $row){
+$query = $query." disciplines LIKE '%".$row."%' OR";	
+					
+			}
+			}
+				if (isset($_POST['timeframe'])){
+				foreach($_POST['timeframe'] as $row){
+$query = $query." timeframe LIKE '%".$row."%' OR";	
+		
+				}
+				}
+				if (isset($_POST['status'])){
+				foreach($_POST['status'] as $row){
+				$query = $query." status LIKE '%".$row."%' OR";	
+		
+				}
+			}
+		
+$or = 'OR';					
+
+    if (substr($query, -strlen($or)) === $or){
+		$state =substr_replace($query ,"",-2);
+//		echo $state;
+	}
+
+			$stmt = $db->prepareStatement($state);
+			$db->executeStatement($stmt);
+			$result = $db->getResult();
+			print_r ($result);	
+			
+		
 	}
 	else{
 		echo "<div class='well'><a href='index.php?content=lead_edit' class='btn btn-large btn-success'>Create a Lead</a></div>";
 		echo "<hr />";
 		
-		// Connect to database
-		$db = new DatabaseHelper();
-		
+		// Connect to database		
 		// Get  category options
 		$sql = "SELECT * FROM CategoryOptions";
 		$stmt = $db->prepareStatement($sql);
@@ -38,7 +123,7 @@
 				<div class="row clearfix">
 					<div class="col-md-2">Community Partner:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="client" size="5">
+						<select multiple="multiple" class="form-control" name="client[]" size="5">
 							<?php
 								foreach($partners as $row){
 									if($row['community_partner'] != NULL)
@@ -50,7 +135,7 @@
 					
 					<div class="col-md-2">Idea Name:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="name" size="5">
+						<select multiple="multiple" class="form-control" name="name[]" size="5">
 							<?php
 								foreach($names as $row){
 									if($row['idea_name'] != NULL)
@@ -64,7 +149,7 @@
 				<div class="row clearfix">
 					<div class="col-md-2">Idea Type:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="type" size="5">
+						<select multiple="multiple" class="form-control" name="type[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['idea_type'] != NULL)
@@ -76,7 +161,7 @@
 				
 					<div class="col-md-2">Possible Program Referral:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="referral" size="5">
+						<select multiple="multiple" class="form-control" name="referral[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['referral'] != NULL)
@@ -90,7 +175,7 @@
 				<div class="row clearfix">
 					<div class="col-md-2">Organization's Mandate:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="mandate" size="5">
+						<select multiple="multiple" class="form-control" name="mandate[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['mandate'] != NULL)
@@ -102,7 +187,7 @@
 					
 					<div class="col-md-2">Focus Area:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="focus" size="5">
+						<select multiple="multiple" class="form-control" name="focus[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['focus'] != NULL)
@@ -116,7 +201,7 @@
 				<div class="row clearfix">
 					<div class="col-md-2">Main Activities:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="activities" size="5">
+						<select multiple="multiple" class="form-control" name="activities[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['main_activities'] != NULL)
@@ -128,7 +213,7 @@
 					
 					<div class="col-md-2">Delivery Location:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="delivery" size="5">
+						<select multiple="multiple" class="form-control" name="delivery[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['delivery_location'] != NULL)
@@ -142,7 +227,7 @@
 				<div class="row clearfix">
 					<div class="col-md-2">Possible Disciplines:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="disciplines" size="5">
+						<select multiple="multiple" class="form-control" name="disciplines[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['disciplines'] != NULL)
@@ -154,7 +239,7 @@
 					
 					<div class="col-md-2">Timeframe:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="timeframe" size="5">
+						<select multiple="multiple" class="form-control" name="timeframe[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['timeframe'] != NULL)
@@ -168,7 +253,7 @@
 				<div class="row clearfix">
 					<div class="col-md-2">Current Status:</div>
 					<div class="col-md-4">
-						<select multiple="multiple" class="form-control" name="status" size="5">
+						<select multiple="multiple" class="form-control" name="status[]" size="5">
 							<?php
 								foreach($categories as $row){
 									if($row['referral'] != NULL)
