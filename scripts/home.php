44de16cf4430b28bc315a18bb17f6e37269c1efc
Kieran Harrison <h7r8@ugrad.cs.ubc.ca>
2014-03-24 09:55:47 -0700

    Minor query fix

--- scripts/home.php
+++ scripts/home.php
@@ -5,23 +5,29 @@
 	include('database_helper.php');
 	
 		$db = new DatabaseHelper();
-	$query = "SELECT idea_name, description FROM `cbel_lead` WHERE `activity_count`>0 ORDER BY `idea_name` desc LIMIT 8";
+	$query = "SELECT lead_name, description FROM 
+					(
+						SELECT lead_name, description FROM `cbel_lead` 
+						WHERE `activity_count` > 0 
+						ORDER BY `activity_count` DESC LIMIT 8
+					) AS T1
+					ORDER BY `lead_name` ASC";
 	
 	$stmt = $db->prepareStatement($query);
 		$db->executeStatement($stmt);
-		$result = $db->getResult();
+		$result = $db->getResult($stmt);
 	
 
-//	print_r($result[0]['idea_name']);
+//	print_r($result[0]['lead_name']);
 //	echo count($result);
 	$int = 0; 
 //	while ($int < count($result)){
 //		if ($int %2 ==0){
 //			echo $int;
-//			echo($result[$int]['idea_name']);
+//			echo($result[$int]['lead_name']);
 //		}else {
 //		echo $int;
-//		echo ($result[$int]['idea_name']);
+//		echo ($result[$int]['lead_name']);
 //		}
 //		$int++;
 //	}
@@ -37,7 +43,7 @@ while ($int < count($result)){
 			<div class="panel panel-primary">
 				<div class="panel-heading">
 					<h3 class="panel-title">'.
-						$result[$int]['idea_name'].'
+						$result[$int]['lead_name'].'
 					</h3>
 				</div>
 				<div class="panel-body">'.
@@ -53,7 +59,7 @@ while ($int < count($result)){
 			<div class="panel panel-primary">
 				<div class="panel-heading">
 					<h3 class="panel-title">'.
-							$result[$int]['idea_name'].'
+							$result[$int]['lead_name'].'
 				</h3>
 				</div>
 				<div class="panel-body">'.
